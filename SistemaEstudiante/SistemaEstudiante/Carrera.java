9c84f807c35544f3573afe2faf5d4a550164c4d6
Edgarjvm <edgarjvm98@gmail.com>
2026-04-05 21:14:55 -0600

    Sistema Estudiante
    version 1.1

---
+++ SistemaEstudiante/SistemaEstudiante/Carrera.java
@@ -0,0 +1,26 @@
+package SistemaEstudiante;
+
+public class Carrera {
+    private String nombre;
+    private String clave;
+
+    public Carrera(String nombre, String clave){
+        this.nombre = nombre;
+        this.clave = clave;
+    }
+    public Carrera(){}
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getClave() {
+        return clave;
+    }
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+    
+    
+}
